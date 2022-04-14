ed2f727ddc8b6abf096a7854f16eb377e92da63b
ggjiny <jnss599@gmail.com>
2022-04-14 22:52:02 +0900

    정적 파일 및 미디어 파일 처리, 블로그 글에 이미지 및 파일 첨부 추가

---
+++ blog_main/blog/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import Post
+
+
+# Create your views here.
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(request, 'blog/post_list.html',{
+#         'posts': posts,
+#     })
+
+
+# def single_post_page(request, pk): #하나만 뽑아오기
+#     post = Post.objects.get(pk = pk)
+#     return render(
+#         request,
+#         'blog/post_detail.html',
+#         {
+#             'post': post,
+#         }
+#     )
+
+
+class PostList(ListView):
+    model = Post
+
+
+class PostDetail(DetailView):
+    model = Post
