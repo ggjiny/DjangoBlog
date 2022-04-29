fafbf6f26429bb99ffad7d48d977d6d9e2243d4b
ggjiny <jnss599@gmail.com>
2022-04-29 16:15:15 +0900

    crispy / markdown 모듈 적용

--- blog_main/blog/admin.py
+++ blog_main/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post, Category, Tag
+from markdownx.admin import MarkdownxModelAdmin
 
+from .models import Post, Category, Tag
 
 # Register your models here.
 
@@ -10,6 +11,6 @@ class CategoryAdmin(admin.ModelAdmin):
 class TagAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}
 
-admin.site.register(Post)
+admin.site.register(Post, MarkdownxModelAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Tag, TagAdmin)
